b1e4859fbf97caffd591678d97a1b39dc8b0cacc
rc924 <monika18>
2020-03-05 09:48:47 -0500

    merge fixes rahil

--- rpthread.c
+++ rpthread.c
@@ -8,27 +8,22 @@
 
 /* INITAILIZE ALL YOUR VARIABLES HERE*/
 
-
 // tcb *head , *tail; //  > might not be able to create global head and tail
 // .*head = *tail = NULL;
 
-boolean first_time_creating = TRUE; // variable used to check if pthread_create has ever been run before
-ucontext_t * uctx_current , * uctx_sched; // current thread context; scheduler context
+boolean first_time_creating = TRUE;    // variable used to check if pthread_create has ever been run before
+ucontext_t *uctx_current, *uctx_sched; // current thread context; scheduler context
 
-mlq * thread_queue; // queue of all threads 
+mlq *ml_queue[4]; // queue of all threads
 struct itimerval mytime;
 
 /* END OF GLOBAL VARIABLE INIT*/
 
-
-
 int main()
 {
 
-	return 0;
+    return 0;
 }
-
-
 
 /* create a new thread */
 /*
@@ -45,110 +40,107 @@ create desired thread
 //don't set the context return function to pthread exit
 */
 int rpthread_create(rpthread_t *thread, pthread_attr_t *attr,
-					void *(*function)(void *), void *arg)
+                    void *(*function)(void *), void *arg)
 {
-	/* Since first time calling then we need to save "main" to tcb block*/
-	// ! REMEMBER TO ADD CURRENT TO QUEUE FOR THE FIRST TIME CAUSE IT CONTAINS MAIN
-	if (first_time_creating){
-		first_time_creating = FALSE; // make sure never run again
-	
-		thread_queue = (mlq*) malloc(sizeof(mlq));
-		thread_queue-> head = NULL;
-		thread_queue-> tail = NULL;
-		
+    /* Since first time calling then we need to save "main" to tcb block*/
+    // ! REMEMBER TO ADD CURRENT TO QUEUE FOR THE FIRST TIME CAUSE IT CONTAINS MAIN
+    if (first_time_creating)
+    {
+        first_time_creating = FALSE; // make sure never run again
 
-		//since first time running, we can assume called from main
-		create_tcb_main();
-		//create scheduler context, but do not make it a tcb block
-		create_scheduler_context();
-		//create timer 
-		init_timer();
+        ml_queue_init();
 
-		// ! DO NOT ENQUEUE main b/c when call scheduler, you will enqueue current context there, which is main
-	}
-	
+        //since first time running, we can assume called from main
+        create_tcb_main();
+        //create scheduler context, but do not make it a tcb block
+        create_scheduler_context();
+        //create timer
+        init_timer();
 
-	//Establish the new thread 
+        // ! DO NOT ENQUEUE main b/c when call scheduler, you will enqueue current context there, which is main
+    }
+
+    //Establish the new thread
     void *thread_stack = malloc(STACK_SIZE);
-	if (thread_stack == NULL ){
-		perror("Failed to allocate stack: Pthread_create(thread_stack)");
-		exit(1);
-	}
-    ucontext_t * uctx_new_thread= (ucontext_t*)malloc(sizeof(ucontext_t));
-    uctx_new_thread->uc_link=NULL;//need to link this something? do not point this to the exit function because the exit function might run twice -DAVID
-    uctx_new_thread->uc_stack.ss_sp=thread_stack;
-    uctx_new_thread->uc_stack.ss_size=STACK_SIZE;
-    uctx_new_thread->uc_stack.ss_flags=0;//dont know what this does
+    if (thread_stack == NULL)
+    {
+        perror("Failed to allocate stack: Pthread_create(thread_stack)");
+        exit(1);
+    }
+    ucontext_t *uctx_new_thread = (ucontext_t *)malloc(sizeof(ucontext_t));
+    uctx_new_thread->uc_link = NULL; //need to link this something? do not point this to the exit function because the exit function might run twice -DAVID
+    uctx_new_thread->uc_stack.ss_sp = thread_stack;
+    uctx_new_thread->uc_stack.ss_size = STACK_SIZE;
+    uctx_new_thread->uc_stack.ss_flags = 0; //dont know what this does
 
-	// check if getcontext fails
-    if (getcontext(uctx_new_thread) < 0){
+    // check if getcontext fails
+    if (getcontext(uctx_new_thread) < 0)
+    {
         perror("getcontext:failure in pthread_create(uctx_new_thread)");
         exit(1);
     }
 
-	// makeconext, based on args
-	if(arg){ // arguments passed
-		makecontext(uctx_new_thread,(void*)function,1,arg);		
-	}
-	else{  // no args given 
-		makecontext(uctx_new_thread,(void*)function,0);
-	}
+    // makeconext, based on args
+    if (arg)
+    { // arguments passed
+        makecontext(uctx_new_thread, (void *)function, 1, arg);
+    }
+    else
+    { // no args given
+        makecontext(uctx_new_thread, (void *)function, 0);
+    }
 
-	// ! ALOT MISSING FROM TCB BLOCK, REVISE LATER
-	// TODO REVISE 
-	tcb * tcb_newthread = (tcb*)malloc(sizeof(tcb)); 
-	tcb_newthread->tid = 0;
-	tcb_newthread->t_status = READY;
-	tcb_newthread->priority = 0;
-	tcb_newthread->t_context = uctx_new_thread;
+    // ! ALOT MISSING FROM TCB BLOCK, REVISE LATER
+    // TODO REVISE
+    tcb *tcb_newthread = (tcb *)malloc(sizeof(tcb));
+    tcb_newthread->tid = 0;
+    tcb_newthread->t_status = READY;
+    tcb_newthread->priority = 0;
+    tcb_newthread->t_context = uctx_new_thread;
 
-	//! ENQUEUE THIS tcb_newthread
+    //! ENQUEUE THIS tcb_newthread
+    enqueue(tcb_newthread);
 
-	/* switch to the scheduler */
-	schedule();
+    /* switch to the scheduler */
+    schedule();
 
-	
-	return 0;
+    return 0;
 };
-
-
-
-
 
 /* give CPU possession to other user-level threads voluntarily */
 // timer reset and signal handler function
 int rpthread_yield()
 {
 
-	// Change thread state from Running to Ready
-	// Save context of this thread to its thread control block
-	// switch from thread context to scheduler context
-	/* 
+    // Change thread state from Running to Ready
+    // Save context of this thread to its thread control block
+    // switch from thread context to scheduler context
+    /* 
 	1. Update some struct value (READY)
 	2. Run getcontext on current context
 	3. stop timer
 	4. switch to scheduler function (i.e. the thread that handles scheduling)
 	*/
 
-	//MAKE SURE NOT WORKING WITH EMPTY LL
-	getitimer(ITIMER_PROF, &mytime);
-	mytime.it_value.tv_usec = 0; // stop the timer
-	setitimer(ITIMER_PROF, &mytime, NULL);
-	swapcontext(uctx_current, uctx_sched ); // save current context, and then switch to scheduler 
+    //MAKE SURE NOT WORKING WITH EMPTY LL
+    getitimer(ITIMER_PROF, &mytime);
+    mytime.it_value.tv_usec = 0; // stop the timer
+    setitimer(ITIMER_PROF, &mytime, NULL);
+    swapcontext(uctx_current, uctx_sched); // save current context, and then switch to scheduler
 
-	return 0;
+    return 0;
 };
 
 /* terminate a thread */
 //
 void rpthread_exit(void *value_ptr){
-	// Deallocated any dynamic memory created when starting this thread
+    // Deallocated any dynamic memory created when starting this thread
 
-	// YOUR CODE HERE
+    // YOUR CODE HERE
 };
 
 /* Wait for thread termination */
-// inside of tcb keep a list of all threads waiting to join  
+// inside of tcb keep a list of all threads waiting to join
 int rpthread_join(rpthread_t thread, void **value_ptr)
 {
 
@@ -161,7 +153,8 @@ int rpthread_join(rpthread_t thread, void **value_ptr)
 
 /* initialize the mutex lock */
 int rpthread_mutex_init(rpthread_mutex_t *mutex,
-                        const pthread_mutexattr_t *mutexattr) {
+                        const pthread_mutexattr_t *mutexattr)
+{
     //Initialize data structures for this mutex
 
     // YOUR CODE HERE
@@ -169,7 +162,8 @@ int rpthread_mutex_init(rpthread_mutex_t *mutex,
 };
 
 /* aquire the mutex lock */
-int rpthread_mutex_lock(rpthread_mutex_t *mutex) {
+int rpthread_mutex_lock(rpthread_mutex_t *mutex)
+{
     // use the built-in test-and-set atomic function to test the mutex
     // When the mutex is acquired successfully, enter the critical section
     // If acquiring mutex fails, push current thread into block list and
@@ -180,7 +174,8 @@ int rpthread_mutex_lock(rpthread_mutex_t *mutex) {
 };
 
 /* release the mutex lock */
-int rpthread_mutex_unlock(rpthread_mutex_t *mutex) {
+int rpthread_mutex_unlock(rpthread_mutex_t *mutex)
+{
     // Release mutex and make it available again.
     // Put threads in block list to run queue
     // so that they could compete for mutex later.
@@ -190,14 +185,16 @@ int rpthread_mutex_unlock(rpthread_mutex_t *mutex) {
 };
 
 /* destroy the mutex */
-int rpthread_mutex_destroy(rpthread_mutex_t *mutex) {
+int rpthread_mutex_destroy(rpthread_mutex_t *mutex)
+{
     // Deallocate dynamic memory created in rpthread_mutex_init
 
     return 0;
 };
 
 /* scheduler */
-static void schedule() {
+static void schedule()
+{
     // Every time when timer interrup happens, your thread library
     // should be contexted switched from thread context to this
     // schedule function
@@ -210,14 +207,17 @@ static void schedule() {
     // else if (sched == MLFQ)
     // 		sched_mlfq();
 
-    while (1) {// is the while loop calling the same sub rountine schedule func over and over?
+    while (1)
+    { // is the while loop calling the same sub rountine schedule func over and over?
 
-        if (sctf_flag) {
+        if (sctf_flag)
+        {
             sched_stcf();
-        } else {
+        }
+        else
+        {
             sched_mlfq();
         }
-
     }
 
 // schedule policy
@@ -228,7 +228,7 @@ static void schedule() {
 #endif
 }
 
-// * UNDELETE THIS STUFF LATER ! 
+// * UNDELETE THIS STUFF LATER !
 // /*  Preemptive SJF (STCF) scheduling algorithm */
 // static void sched_stcf()
 // {
@@ -238,7 +238,6 @@ static void schedule() {
 // 	// YOUR CODE HERE
 // 	return;  // ! delete later
 // }
-
 
 // /* Preemptive MLFQ scheduling algorithm */
 // static void sched_mlfq()
@@ -250,209 +249,219 @@ static void schedule() {
 // 	return;  // ! delete later
 // }
 
-
-
-
 // Feel free to add any other functions you need
-
-
-
-
-
 
 // ! YOUR CODE HERE !
 
+void create_scheduler_context()
+{
 
-void create_scheduler_context(){
+    ucontext_t *uctx_sched = (ucontext_t *)malloc(sizeof(ucontext_t));
 
-	
-	ucontext_t * uctx_sched= (ucontext_t*)malloc(sizeof(ucontext_t));
+    /*create stack and check if memory allocation good*/
+    void *stack_sched = malloc(STACK_SIZE);
 
-	/*create stack and check if memory allocation good*/
-	void *stack_sched = malloc(STACK_SIZE);
+    if (stack_sched == NULL)
+    {
+        perror("Failed to allocate stack");
+        exit(1);
+    }
+    // use getcontext to avoid segfault
+    if (getcontext(uctx_sched) < 0)
+    {
+        perror("getcontext");
+        exit(1);
+    }
 
-	if (stack_sched == NULL)	{
-		perror("Failed to allocate stack");
-		exit(1);
-	}
-	// use getcontext to avoid segfault
-	if (getcontext(uctx_sched) < 0){
-		perror("getcontext");
-		exit(1);
-	}
+    // set the necessary values
+    uctx_sched->uc_stack.ss_sp = stack_sched;
+    uctx_sched->uc_stack.ss_size = STACK_SIZE;
+    uctx_sched->uc_stack.ss_flags = 0;
+    uctx_sched->uc_link = NULL; // it will switch to that context, but since null will exit
 
-	// set the necessary values 
-	uctx_sched->uc_stack.ss_sp = stack_sched;
-	uctx_sched->uc_stack.ss_size = STACK_SIZE;
-	uctx_sched->uc_stack.ss_flags = 0; 
-	uctx_sched->uc_link  = NULL; // it will switch to that context, but since null will exit 
-
-	// make the context for future use 
-	makecontext(uctx_sched, (void *)&schedule,0);
+    // make the context for future use
+    makecontext(uctx_sched, (void *)&schedule, 0);
 }
 
+void create_tcb_main()
+{
+    tcb *tcb_main = (tcb *)malloc(sizeof(tcb));
+    tcb_main->tid = 0;
+    tcb_main->t_status = RUNNING;
+    tcb_main->priority = 0;
 
+    /*Creating the threadControlBlock(tcb)  */
+    ucontext_t *uctx_main = (ucontext_t *)malloc(sizeof(ucontext_t));
 
-void create_tcb_main(){
-	tcb * tcb_main = (tcb*)malloc(sizeof(tcb)); 
-	tcb_main->tid = 0;
-	tcb_main->t_status = RUNNING;
-	tcb_main->priority = 0;
+    // save context of main
+    if (getcontext(uctx_main) < 0)
+    {
+        perror("getcontext: create_tcb_main");
+        exit(1);
+    }
+    uctx_current = uctx_main; // make current context point to main
 
-	/*Creating the threadControlBlock(tcb)  */
-	ucontext_t *uctx_main = (ucontext_t*)malloc(sizeof(ucontext_t));
-
-	// save context of main
-	if (getcontext(uctx_main) < 0){
-		perror("getcontext: create_tcb_main");
-		exit(1);
-	}
-	uctx_current = uctx_main; // make current context point to main
-
-
-	tcb_main->t_context = uctx_main;
+    tcb_main->t_context = uctx_main;
 }
 
-void init_timer(){
-	memset(&mytime, 0 , sizeof(mytime));
-	mytime.it_value.tv_usec = 0;
-	mytime.it_value.tv_sec = 0;
-    mytime.it_interval.tv_usec = 0; 
+void init_timer()
+{
+    memset(&mytime, 0, sizeof(mytime));
+    mytime.it_value.tv_usec = 0;
+    mytime.it_value.tv_sec = 0;
+    mytime.it_interval.tv_usec = 0;
     mytime.it_interval.tv_sec = 0;
-	setitimer(ITIMER_PROF, &mytime, NULL); 
+    setitimer(ITIMER_PROF, &mytime, NULL);
 }
 
+void enqueue(tcb *tcb_node)
+{
 
-
-void enqueue(tcb *tcb_node) {
-
-    if (sctf_flag) {
-        if (ml_queue[0]->head == NULL) {
+    if (sctf_flag)
+    {
+        if (ml_queue[0]->head == NULL)
+        {
             ml_queue[0]->head = tcb_node;
             ml_queue[0]->tail = tcb_node;
         }
-        else {
+        else
+        {
             ml_queue[0]->tail->next = tcb_node;
             ml_queue[0]->tail = tcb_node;
         }
     }
-    else {//MLFQ algo
+    else
+    { //MLFQ algo
         int quant = tcb_node->quantum;
-        int level=get_level(quant);
+        int level = get_level(quant);
 
-        if(ml_queue[level]==NULL){
+        if (ml_queue[level] == NULL)
+        {
             ml_queue[level]->head = tcb_node;
             ml_queue[level]->tail = tcb_node;
         }
-        else{
+        else
+        {
             ml_queue[level]->tail->next = tcb_node;
             ml_queue[level]->tail = tcb_node;
         }
     }
-
 }
 
-
-
-
-
-tcb *tcb_init(ucontext_t *cctx, rpthread_t id) {
-    tcb *newNode = (tcb *) malloc(sizeof(tcb));
+tcb *tcb_init(ucontext_t *cctx, rpthread_t id)
+{
+    tcb *newNode = (tcb *)malloc(sizeof(tcb));
     newNode->tid = id;
     //NEED TO SET STATUS
     newNode->t_context = cctx;
-    newNode->priority = 0;//default val
+    newNode->priority = 0; //default val
     //need stack?
     newNode->next = NULL;
     return newNode;
 }
 // YOUR CODE HERE
 
-
-
-int getQueueSize(tcb* head) {//not super good method since has to pass whole time to check size
+int getQueueSize(tcb *head)
+{ //not super good method since has to pass whole time to check size
     tcb *curr = head;
     int size = 0;
-    while (curr != NULL) {
+    while (curr != NULL)
+    {
         size++;
         curr = curr->next;
     }
     return size;
 }
 
-void printQueue(tcb* head) {
+void printQueue(tcb *head)
+{
     tcb *curr = head;
 
-    while (curr != NULL) {
+    while (curr != NULL)
+    {
         printf("%d\t", curr->tid);
         curr = curr->next;
     }
-    printf("\n");//new line
+    printf("\n"); //new line
 }
 
-tcb *dequeue() {
+tcb *dequeue()
+{
 
-    if(sctf_flag){
-        head=ml_queue[0]->head;
-        if(getQueueSize(head)!=0){
-            tcb* first=head;
-            head=head->next;
+    if (sctf_flag)
+    {
+        head = ml_queue[0]->head;
+        if (getQueueSize(head) != 0)
+        {
+            tcb *first = head;
+            head = head->next;
             return first;
         }
-        else{
+        else
+        {
             puts("cant dequeue from empty Q");
             return NULL;
         }
     }
 
-   else{//MLFQ
-       tcb * first;
-       int i=0;
-       for(i;i<LEVELS;i++){
-           mlq * curr_q=ml_queue[i];
-           if(curr_q->head!=NULL){
-               first=curr_q->head;
-               curr_q->head=curr_q->head->next;
-               return first;
-           }
-       }
-       puts("all levels are empty");
-       return NULL;
-
-
-   }
+    else
+    { //MLFQ
+        tcb *first;
+        int i = 0;
+        for (i; i < LEVELS; i++)
+        {
+            mlq *curr_q = ml_queue[i];
+            if (curr_q->head != NULL)
+            {
+                first = curr_q->head;
+                curr_q->head = curr_q->head->next;
+                return first;
+            }
+        }
+        puts("all levels are empty");
+        return NULL;
+    }
 }
 
-void ml_queue_init() {
+void ml_queue_init()
+{
 
-    if (sctf_flag == 0) {
+    if (sctf_flag == 0)
+    {
         int i = 0;
-        for (i; i < LEVELS; i++) {
-            ml_queue[i] = (mlq *) malloc(sizeof(mlq));
+        for (i; i < LEVELS; i++)
+        {
+            ml_queue[i] = (mlq *)malloc(sizeof(mlq));
             ml_queue[i]->head = NULL;
             ml_queue[i]->tail = NULL;
             i++;
         }
-    } else {
-        ml_queue[0] = (mlq *) malloc(sizeof(mlq));
+    }
+    else
+    {
+        ml_queue[0] = (mlq *)malloc(sizeof(mlq));
         ml_queue[0]->head = NULL;
         ml_queue[0]->tail = NULL;
         //rest indexes should be null but will need to check
     }
 }
 
-
-int get_level(int quant){
-    if(quant<=L1){
+int get_level(int quant)
+{
+    if (quant <= L1)
+    {
         return 0;
     }
-    else if(quant>L1 && quant<=L2){
+    else if (quant > L1 && quant <= L2)
+    {
         return 1;
     }
-    else if (quant>L2 && quant<=L3){
+    else if (quant > L2 && quant <= L3)
+    {
         return 2;
     }
-    else{//L4
+    else
+    { //L4
         return 3;
     }
 }
